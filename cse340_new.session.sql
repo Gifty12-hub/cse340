36226ac195e36dc8d83abae255e4689ee00fde04
Giftyarkoh <garkoh@byupathway.edu>
2026-03-15 22:40:45 +0000

    assign

---
+++ cse340_new.session.sql
@@ -0,0 +1,5 @@
+
+-- Query 6 from assignment2
+UPDATE inventory
+SET inv_image = REPLACE(inv_image,'/images/','/images/vehicles/'),
+inv_thumbnail = REPLACE(inv_thumbnail,'/images/','/images/vehicles/');
