e52c28f775b54630438fcd40fc706772b51ec9ba
Giftyarkoh <garkoh@byupathway.edu>
2026-03-15 23:23:17 +0000

    week 3a

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const baseController = require("./controllers/baseController")
 
 /* ***********************
  * Veiw Engine and Templates
@@ -29,6 +30,7 @@ app.use(static)
 app.get("/", function(req, res){
   res.render("index", { title: "Home" })
 })
+app.get("/", baseController.buildHome)
 
 /* ***********************
  * Local Server Information
