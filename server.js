519e6a4b17a0a1fc2b2c28bd4f656384076bc05f
Giftyarkoh <garkoh@byupathway.edu>
2026-03-26 23:34:20 +0000

    week 4 commit

--- server.js
+++ server.js
@@ -13,6 +13,22 @@ const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
 const utilities = require("./utilities");
+const session = require("express-session")
+const pool = require('./database/')
+
+/* ***********************
+ * Middleware
+ * ************************/
+ app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
 
 /* ***********************
  * View Engine and Templates
@@ -46,6 +62,7 @@ app.get("/test", (req, res) => {
 app.use(async (req, res, next) => {
   next({status: 404, message: 'Sorry, we appear to have lost that page.'})
 })
+
 /* ***********************
 * Express Error Handler*/
 app.use(async (err, req, res, next) => {
@@ -73,3 +90,10 @@ app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
  
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
