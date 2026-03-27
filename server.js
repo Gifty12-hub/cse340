68eb5693b3878504cea01119d0aec2711a610f2a
Giftyarkoh <garkoh@byupathway.edu>
2026-03-27 19:51:46 +0000

    week4a

--- server.js
+++ server.js
@@ -91,7 +91,7 @@ app.listen(port, () => {
 })
  
 
-// Express Messages Middleware
+/* Express Messages Middleware */
 app.use(require('connect-flash')())
 app.use(function(req, res, next){
   res.locals.messages = require('express-messages')(req, res)
